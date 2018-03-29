e8159d94515ca720984a8e85c9e5ea6247675721
Aleksey Sidorenko <sidorenko.aleksey@gmail.com>
2018-03-30 00:10:34 +1000

    Task #35008 - Компаратор для строк (refactoring).

--- chapter_003/src/main/java/ru/job4j/collections/comparable/ListCompare.java
+++ chapter_003/src/main/java/ru/job4j/collections/comparable/ListCompare.java
@@ -1,8 +1,6 @@
 package ru.job4j.collections.comparable;
 
-import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.List;
 
 /**
  * Class ListCompare Решение задачи: Компаратор для строк. [#35008]
@@ -19,25 +17,16 @@ public class ListCompare implements Comparator<String> {
      */
     @Override
     public int compare(String left, String right) {
-        List<Character> leftList = new ArrayList<>();
-        List<Character> rightList = new ArrayList<>();
         int result = 0;
-        char[] array = left.toCharArray();
-        for (char symbol : array) {
-            leftList.add(symbol);
-        }
-        array = right.toCharArray();
-        for (char symbol : array) {
-            rightList.add(symbol);
-        }
-        for (int i = 0; (i < leftList.size()) && (i < rightList.size()); i++) {
-            result = Integer.compare(leftList.get(i), rightList.get(i));
-            if ((leftList.get(i) != rightList.get(i))) {
+
+        for (int i = 0; (i < left.length()) && (i < right.length()); i++) {
+            result = Integer.compare(left.charAt(i), right.charAt(i));
+            if (left.charAt(i) != right.charAt(i)) {
                 break;
             }
         }
-        if ((result == 0) && (leftList.size() != rightList.size())) {
-            result = Integer.compare(leftList.size(), rightList.size());
+        if ((result == 0) && (left.length() != right.length())) {
+            result = Integer.compare(left.length(), right.length());
         }
         return result;
     }
