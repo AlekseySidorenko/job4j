e2d04af6ccdaf2c483cfbeea5349ae5bc22e92ac
Aleksey Sidorenko <sidorenko.aleksey@gmail.com>
2018-05-07 20:53:38 +1000

    Task #151 - Make prime numbers iterator (refactoring).

--- chapter_004/src/main/java/ru/job4j/iterator/PrimeIterator.java
+++ chapter_004/src/main/java/ru/job4j/iterator/PrimeIterator.java
@@ -22,50 +22,44 @@ public class PrimeIterator implements Iterator {
     }
 
     /**
+     * Check value for prime.
+     * @param value value.
+     * @return prime is true, not prime is false.
+     */
+    private boolean isPrime(int value) {
+        int numberOfDividers = 0;
+        for (int i = 1; i <= value; i++) {
+            if ((value % i) == 0) {
+                numberOfDividers++;
+            }
+        }
+        return numberOfDividers == MAX_NUMBER_OF_DIVIDERS;
+    }
+
+    /**
      * Check next element presence in array.
      * @return presence is true, not presence is false.
      */
     @Override
     public boolean hasNext() {
-        boolean result = false;
-        int i = index;
-        while (i < numbers.length) {
-            int numberOfDividers = 0;
-            for (int j = 1; j <= numbers[i]; j++) {
-                if ((numbers[i] % j) == 0) {
-                    numberOfDividers++;
-                }
+        while (index < numbers.length) {
+            if (isPrime(numbers[index])) {
+                return true;
             }
-            if (numberOfDividers == MAX_NUMBER_OF_DIVIDERS) {
-                result = true;
-                break;
-            }
-            i++;
+            index++;
         }
-        return result;
+        return false;
     }
 
     /**
      * Get next element in array.
      * @return next element in array.
-     * @throws NoSuchElementException
+     * @throws NoSuchElementException NoSuchElementException
      */
     @Override
     public Object next() {
         if (!this.hasNext()) {
             throw new NoSuchElementException();
-        }
-        while ((this.hasNext())) {
-            int numberOfDividers = 0;
-            for (int i = 1; i <= numbers[index]; i++) {
-                if ((numbers[index] % i) == 0) {
-                    numberOfDividers++;
-                }
-            }
-            if (numberOfDividers == MAX_NUMBER_OF_DIVIDERS) {
-                break;
-            }
-            index++;
         }
         return numbers[index++];
     }
