75ab803a00ed30e27e92cf576c9d5667d10dedef
Aleksey Sidorenko <sidorenko.aleksey@gmail.com>
2019-04-15 15:27:28 +1000

    Task #859 - Analyze server availability (test fix).

--- chapter_006/src/test/java/ru/job4j/io/AnalyzeTest.java
+++ chapter_006/src/test/java/ru/job4j/io/AnalyzeTest.java
@@ -2,9 +2,7 @@ package ru.job4j.io;
 
 import org.junit.Test;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 import static org.junit.Assert.assertEquals;
 
@@ -19,21 +17,30 @@ public class AnalyzeTest {
      * Test unavailable().
      */
     @Test
-    public void whenReadLogFileThenGetAnavailablePeriods() {
+    public void whenReadLogFileThenGetAnavailablePeriods() throws Exception {
+
+        String rootPath = System.getProperty("java.io.tmpdir");
+
+        File sourceData = new File(rootPath + "\\sourcelog.txt");
+        PrintWriter sourceLog = new PrintWriter(sourceData);
+        sourceLog.println("200 10:56:01");
+        sourceLog.println("500 10:57:01");
+        sourceLog.println("400 10:58:01");
+        sourceLog.println("200 10:59:01");
+        sourceLog.println("500 11:01:02");
+        sourceLog.println("200 11:02:02");
+        sourceLog.close();
+
         Analyze analyze = new Analyze();
-        String source = "d:\\Dropbox\\develop\\projects\\aleksey_sidorenko\\"
-                      + "chapter_006\\src\\test\\java\\ru\\job4j\\io\\sourcelog.txt";
-        String target = "d:\\Dropbox\\develop\\projects\\aleksey_sidorenko\\"
-                      + "chapter_006\\src\\test\\java\\ru\\job4j\\io\\target.csv";
+        String source = sourceData.getPath();
+        String target = rootPath + "\\target.csv";
 
         analyze.unavailable(source, target);
         String expected = "10:57:01;10:59:01;";
-        String result = "";
+        String result;
 
-        try (BufferedReader sourceLog = new BufferedReader(new FileReader(target))) {
-            result = sourceLog.readLine();
-        } catch (IOException e) {
-            e.printStackTrace();
+        try (BufferedReader br = new BufferedReader(new FileReader(target))) {
+            result = br.readLine();
         }
         assertEquals(expected, result);
     }
