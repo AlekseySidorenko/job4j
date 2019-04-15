6e5d340c1ccae8a2ebc63f2c83b4785448c411b1
Aleksey Sidorenko <sidorenko.aleksey@gmail.com>
2019-04-15 14:47:59 +1000

    Task #858 - Reading configuration file.

--- chapter_006/src/test/java/ru/job4j/io/ConfigTest.java
+++ chapter_006/src/test/java/ru/job4j/io/ConfigTest.java
@@ -17,8 +17,7 @@ public class ConfigTest {
      */
     @Test
     public void whenReadEmptyFileThenMapIsEmpty() {
-        Config config = new Config("d:\\Dropbox\\develop\\projects\\aleksey_sidorenko\\"
-                                       + "chapter_006\\src\\test\\java\\ru\\job4j\\io\\empty.app.properties");
+        Config config = new Config("config.empty.app.properties");
         config.load();
         assertTrue(config.getValues().isEmpty());
     }
@@ -28,8 +27,7 @@ public class ConfigTest {
      */
     @Test
     public void whenReadRealFileThenMapGotData() {
-        Config config = new Config("d:\\Dropbox\\develop\\projects\\aleksey_sidorenko\\"
-                                      + "chapter_006\\src\\test\\java\\ru\\job4j\\io\\app.properties");
+        Config config = new Config("config.app.properties");
         String sampleKeyFromFile = "hibernate.connection.driver_class";
         String sampleValueFromFIle = "org.postgresql.Driver";
         config.load();
