f1e775bc66e980f72c9ed388c6cc314e50d0fe0a
Aleksey Sidorenko <sidorenko.aleksey@gmail.com>
2019-09-26 21:51:34 +1000

    Fix dependency.

--- chapter_011/src/main/java/ru/job4j/servlets/EchoServlet.java
+++ chapter_011/src/main/java/ru/job4j/servlets/EchoServlet.java
@@ -5,7 +5,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import jdk.internal.dynalink.linker.LinkerServices;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
